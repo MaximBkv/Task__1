f31d650a3ad68762df45f11868e066b01a9b2a5a
Max <37188858+MaximBkv@users.noreply.github.com>
2020-12-30 02:53:02 +0300

    Add files via upload

---
+++ include/clEdge.h
@@ -0,0 +1,23 @@
+#ifndef CLEDGE_H
+#define CLEDGE_H
+#include "clLine.h"
+#include <cstdlib>
+#include <graphics.h>
+
+class clEdge:public clLine
+{
+    public:
+        clEdge();
+        virtual ~clEdge();
+
+        char* Getw() { return w; }
+        void Setw(int val) { itoa(val, w, 10);}
+        void Show();
+
+    protected:
+
+    private:
+        char* w;
+};
+
+#endif
